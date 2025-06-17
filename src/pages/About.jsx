b75e2ff283751abc86beb719d77cd35975bef179
Oliver2828 <oliverezenwaka95@gmail.com>
2025-06-17 12:34:15 +0100

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,16 @@
-import React from 'react'
-<<<<<<< HEAD
-import About2 from '../components/About-Component/About2'
-import About3 from '../components/About-Component/About3'
-
+import React from 'react';
+import HeroSlider from '../components/About-component/HeroSlider';
+import About2 from '../components/About-Component/About2';
+import About3 from '../components/About-Component/About3';
 
 function About() {
   return (
     <div>
-   <About2/>
-   <About3/>
+      <HeroSlider />
+      <About2 />
+      <About3 />
     </div>
-=======
-import HeroSlider from '../components/About-component/HeroSlider'
-
-function About() {
-  return (
-    <HeroSlider/>
->>>>>>> 3b839dbc406a687d0419939a2f0b60bc6676bd8d
-  )
+  );
 }
 
-export default About
+export default About;
