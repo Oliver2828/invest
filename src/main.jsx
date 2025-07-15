623fc57ff7c8b6b9d294b6d7b35f99fe55d7ae72
kingsLib5 <libertykings88@gmail.com>
2025-07-15 15:12:45 +0100

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-// === src/main.jsx ===
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './index.css';
+import "./index.css";
 import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <div className="w-screen h-screen overflow-x-hidden overflow-y-auto">
+      <App />
+    </div>
   </React.StrictMode>
 );
